a4f2c7d6c6948748292c5c20c2dc96825af236ee
Meghan <meghan.frisco@gmail.com>
2016-12-18 19:52:26 -0500

    Update winter.js

--- winter.js
+++ winter.js
@@ -16,7 +16,7 @@ function revert() {
 		document.getElementById('msg').style.background = "tomato";
 		document.getElementById('msg').style.color = "#ffffff"; 
 		document.getElementById('msg').style.border = "10px double #ffffff";
-		document.getElementById('msg').style.fontSize = "66pt";
+		document.getElementById('msg').style.fontSize = "60pt";
 		document.getElementById('msg').style.overflow = "hidden";
 }
 
